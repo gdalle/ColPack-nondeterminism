0db795d0c639fc4723bddb4e1d31f9d19addc824
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-03-20 14:32:08 +0100

    Actual stuff

---
+++ scripts/install_colpack.sh
@@ -0,0 +1,5 @@
+# https://github.com/CSCsw/ColPack/tree/master?tab=readme-ov-file#try-colpack-by-compile-and-run-without-installation
+git clone https://github.com/CSCsw/ColPack.git   #Download ColPack
+cd ColPack/Examples/ColPackAll       # go to ColPack Example folder
+make                         # compile the code
+cd ../../..
--- scripts/run_colpack.sh
+++ scripts/run_colpack.sh
@@ -1,1 +1,5 @@
-echo "hello"
+for i in $(seq 1 10);
+do
+    echo $i
+    ./ColPack/Examples/ColPackAll/ColPack -f ../data/lp_ken_11.mtx -o NATURAL -m COLUMN_PARTIAL_DISTANCE_TWO
+done
